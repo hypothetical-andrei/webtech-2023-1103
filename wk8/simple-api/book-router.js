b080f97c3d5e1a6ae6129d80a0f73b3e6c49e90f
AT <hypothetical.andrei@gmail.com>
2023-11-14 14:43:25 +0200

    .

---
+++ wk8/simple-api/book-router.js
@@ -0,0 +1,60 @@
+import express from 'express'
+
+const router = express.Router()
+
+// get /books
+// get /books?sortField=title&sortOrder=1
+// http://localhost:8080/books?sortField=content&sortOrder=-1
+router.get('/books', (req, res, next) => {
+  let { sortField, sortOrder } = req.query
+  if (sortField) {
+    sortOrder = parseInt(sortOrder)
+    const bookComparator = (a, b) => {
+      if (a[sortField] < b[sortField]) {
+        return -1 * sortOrder
+      } else {
+        if (a[sortField] > b[sortField]) {
+          return 1 * sortOrder
+        } else {
+          return 0
+        }
+      }
+    }
+    res.status(200).json(res.app.locals.books.sort(bookComparator))
+  } else {
+    res.status(200).json(res.app.locals.books)
+  }
+})
+
+router.post('/books', (req, res, next) => {
+  const book = req.body
+  try {
+    if (book && book.id) {
+      const existing = res.app.locals.books.find(e => e.id === book.id)
+      if (existing) {
+        // res.status(400).json({ message: 'cannot add existing book' })
+        throw new Error('duplicate book id')
+      } else {
+        app.locals.books.push(book)
+        res.status(201).json({ message: 'created book' })
+      }
+    } else {
+      res.status(400).json({ message: 'invalid request' })
+    }    
+  } catch (error) {
+    next(error)
+  }
+})
+
+// get http://localhost:8080/books/1
+router.get('/books/:id', (req, res, next) => {
+  const id = parseInt(req.params.id)
+  const index = res.app.locals.books.findIndex(e => e.id === id)
+  if (index === -1) {
+    res.status(404).json({ message: 'not found' })
+  } else {
+    res.status(200).json(res.app.locals.books[index])
+  }
+})
+
+export default router
--- wk8/simple-api/server.js
+++ wk8/simple-api/server.js
@@ -1,23 +1,35 @@
 import express from 'express'
+import bookRouter from './book-router.js'
 
 const app = express()
+
+app.use((req, res, next) => {
+  console.log(req.url)
+  // res.status(500).json({ message: 'not home now' })
+  next()
+})
+
+app.use(express.json())
 
 app.locals.books = [{
   id: 1,
   title: 'a',
-  content: 'content-a'
+  content: 'a-content'
 }, {
   id: 2,
   title: 'b',
-  content: 'content-a'
+  content: 'b-content'
 }, {
   id: 3,
   title: 'c',
-  content: 'content-a'
+  content: 'c-content'
 }]
 
-app.get('/books', (req, res) => {
-  res.status(200).json(app.locals.books)
+app.use('/', bookRouter)
+
+app.use((err, req, res, next) => {
+  console.log('an error occured')
+  res.status(500).json({ message: 'server error' })
 })
 
 app.listen(8080)
