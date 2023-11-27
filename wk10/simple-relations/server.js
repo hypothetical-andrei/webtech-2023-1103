29de9be23e47c9c5132db28ccf7917cdfa86f2fa
AT <hypothetical.andrei@gmail.com>
2023-11-27 14:36:22 +0200

    .

--- wk10/simple-relations/server.js
+++ wk10/simple-relations/server.js
@@ -69,6 +69,72 @@ app.get('/universities/:uid/students', async (req, res, next) => {
   }
 })
 
+app.get('/universities/:uid/students/:sid', async (req, res, next) => {
+  try {
+    const university = await University.findByPk(req.params.uid, {
+      include: {
+        model: Student,
+        where: {
+          id: req.params.sid
+        }
+      }
+    })
+    if (university) {
+      const student = university.students.shift()
+      if (student) {
+        res.status(200).json(student)
+      } else {
+        res.status(404).json({ message: 'not found' })
+
+      }
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+app.put('/universities/:uid/students/:sid', async (req, res, next) => {
+  try {
+    const results = await Student.update(req.body, {
+      where: {
+        id: req.params.sid,
+        universityId: req.params.uid
+      },
+      fields: ['name', 'email']
+    })
+    console.warn(results)
+    if (results.shift()) {
+        res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.delete('/universities/:uid/students/:sid', async (req, res, next) => {
+  try {
+    const results = await Student.destroy({
+      where: {
+        id: req.params.sid,
+        universityId: req.params.uid
+      }
+    })
+    if (results) {
+        res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
 app.use((err, req, res, next) => {
   console.warn(err)
   res.status(500).json({ message: 'error occured' })
